667fb1e5e513e70accb7e793f872ba0219feb4ed
Gabriel Trigueiro <63242633+Yatmish@users.noreply.github.com>
2021-05-13 06:38:29 -0300

    separete-webScraping
    It'll make the process easier.

--- calc/grahamFormula.py
+++ calc/grahamFormula.py
@@ -1,30 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
-from math import sqrt
 import concurrent.futures
+from math import sqrt
+from web.gettingAllStocks import getBrStocks
 
 # Some configuration for Resquests work and make the propely request
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
 }
-
-# Getting Brazilian Stocks registered in https://fundamentus.com.br
-
-
-def getBrStocks():
-    apiEnterprises = requests.get(
-        'https://fundamentus.com.br/detalhes.php?papel=', headers=headers).text
-
-    soup = BeautifulSoup(apiEnterprises, 'html.parser')
-
-    # find the element that containts the tickets
-    listPapers = list(map(lambda stock: stock.a.text, soup.find_all('tr')))
-
-    filteredOrdinary = list(
-        filter(lambda paper: paper.find("3") != -1, listPapers))  # filtering to only get Ordinary tickets (XXXX3)
-
-    return filteredOrdinary
-
 
 stocks = getBrStocks()
 
